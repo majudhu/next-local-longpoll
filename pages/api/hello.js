6cc399bf1e0a5b7d1c420714c2f39ec8bb6cf835
majudhu <majudhu.sup@gmail.com>
2021-10-09 00:32:27 +0500

    add code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+let messages = [];
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+export default async function (req, res) {
+  switch (req.method) {
+    case "GET":
+      const start = new Date();
+      while (
+        messages.length <= parseInt(req.query.q) &&
+        new Date() - start < 25000
+      ) {
+        await new Promise((r) => setTimeout(r, 100));
+      }
+      return res.json(messages);
+
+    case "POST":
+      messages.push(req.body.message);
+      return res.status(204).send();
+  }
 }
